2315b10e500fd72b8ed2f840b61f2d8040270be2
Edgar1579 <es535965@gmail.com>
2023-09-19 22:06:21 -0500

    creamos template y views en nuestro proyecto

---
+++ base/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def inicio(request):
+    titulo = "inicio"
+    nombre = "Edgar Eduardo"
+    context={
+        "titulo": titulo,
+        "nombre": nombre,
+    }
+    return render(request, "index.html", context)
+
+def contacto(request):
+    titulo
+
+
